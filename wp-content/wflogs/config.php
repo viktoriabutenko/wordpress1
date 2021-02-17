edd4243f384b638d300260330022af2fc001c3c8
viktoriabutenkoi <viktoria.butenko@khk.ee>
2021-02-17 15:53:56 +0200

    modified file `wflogs` (after activation of `Wordfence Security` version 7.4.14)

---
+++ wp-content/wflogs/config.php
@@ -0,0 +1,6 @@
+<?php exit('Access denied'); __halt_compiler(); ?>
+******************************************************************
+This file is used by the Wordfence Web Application Firewall. Read 
+more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
+******************************************************************
+a:4:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1614174830;s:7:"authKey";s:64:"B;8p4xUcmt,O7oSwEy9%]oEbE .{t=G]8UOfU A[XUXg=V*rgoP5d]lcXjh/ngW,";}
