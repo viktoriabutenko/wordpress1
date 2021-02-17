0930cd7d1ddda5cdacfdb50efcb84ae500168186
viktoriabutenkoi <viktoria.butenko@khk.ee>
2021-02-17 15:58:45 +0200

    modified file `config.php`

--- wp-content/wflogs/config.php
+++ wp-content/wflogs/config.php
@@ -3,4 +3,4 @@
 This file is used by the Wordfence Web Application Firewall. Read 
 more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
 ******************************************************************
-a:4:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1614174830;s:7:"authKey";s:64:"B;8p4xUcmt,O7oSwEy9%]oEbE .{t=G]8UOfU A[XUXg=V*rgoP5d]lcXjh/ngW,";}
+a:7:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1614174830;s:7:"authKey";s:64:"B;8p4xUcmt,O7oSwEy9%]oEbE .{t=G]8UOfU A[XUXg=V*rgoP5d]lcXjh/ngW,";s:7:"version";s:5:"1.0.4";s:11:"wafDisabled";b:0;s:13:"attackDataKey";i:449;}
