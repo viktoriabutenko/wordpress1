81eab1d00b99abfe7f0c22ebababe2fec09aa687
viktoriabutenkoi <viktoria.butenko@khk.ee>
2021-03-03 13:15:36 +0200

    modified plugin `Very First Plugin` version 1.0 (after activation of `Very First Plugin` version 1.0)

---
+++ wp-content/plugins/very-first-plugin/very-first-plugin.php
@@ -0,0 +1,9 @@
+<?php
+/**
+* Plugin Name: Very First Plugin
+* Plugin URI: http://lyotsakoolitused.ikt.khk.ee/
+* Description: This is the very first plugin I ever created.
+* Version: 1.0
+* Author: Ly Otsa
+* Author URI: http://lyotsakoolitused.ikt.khk.ee/
+**/
