3ee44ebb55da96c9c2fb1064a21c154805093d87
viktoriabutenkoi <viktoria.butenko@khk.ee>
2021-03-03 13:37:02 +0200

    modified plugin `Very First Plugin` version 1.0

--- wp-content/plugins/very-first-plugin/very-first-plugin.php
+++ wp-content/plugins/very-first-plugin/very-first-plugin.php
@@ -7,3 +7,9 @@
 * Author: Ly Otsa
 * Author URI: http://lyotsakoolitused.ikt.khk.ee/
 **/
+
+function dh_modify_read_more_link() {    
+return '<a class="more-link" href="' . get_permalink() . '">Click to Read!
+</a>';
+}
+add_filter( 'the_content_more_link', 'dh_modify_read_more_link' );
